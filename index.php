524d372c9cde5a1d468eea2c8b97d552f607f6b8
venediktos <ithakipatal@gmail.com>
2024-09-21 14:56:59 +0300

    Project

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>MS Teams - empty test page</title>
+  </head>
+  <body>
+  <h1> Header </h1>
+    <!-- This is an empty test page used in the cypress based scenario tests in TMP -->
+  </body>
+</html>
